03ff9e3c3cba57f9ef0346aefaeebc1093f2b55d
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-03-24 15:38:46 +0100

    intial commit

---
+++ src/main/java/com/cydeo/TicketingHtmlTrialApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingHtmlTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingHtmlTrialApplication.class, args);
+    }
+
+}
