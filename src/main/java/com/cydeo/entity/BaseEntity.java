a0229c33522a9d3a64addf9b96a5b9d34fd19743
Betul Zora <zorabtl@gmail.com>
2023-11-29 11:39:18 -0600

    finished th nullable and updatable attributes of @Column

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -19,21 +16,34 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // use attributes of the @Column annotation to prevent update of the initial creation date.
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
     // use this field for soft delete
     private Boolean isDeleted = false;
 
-    // use this to set BaseEnetity fields
+    // use these methods to set BaseEntity fields
+    @PrePersist // This is what to assign on new entries in DB tables
     public void onPrePersist(){
         this.insertDateTime = LocalDateTime.now();
         this.lastUpdateDateTime = LocalDateTime.now();
 
         // these fields will be elaborated later.
         this.insertUserId = 1L;
+        this.lastUpdateUserId = 1L;
+    }
+
+    @PreUpdate // This method will run when updating a field.
+    public void onPreUpdate(){
+        this.lastUpdateDateTime = LocalDateTime.now();
         this.lastUpdateUserId = 1L;
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
-INSERT INTO roles(description) VALUES('Admin');
-INSERT INTO roles(description) VALUES('Manager');
-INSERT INTO roles(description) VALUES('Employee');
+insert into roles(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, description)
+VALUES ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Admin'),
+('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Manager'),
+('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Employee');
+
+
+insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
+                  first_name, gender, last_name, user_name, role_id)
+values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@admin.com',
+        1);
+
+-- These queries were used in the initial parts for the project to supply the roles
+--INSERT INTO roles(description) VALUES('Admin');
+--INSERT INTO roles(description) VALUES('Manager');
+--INSERT INTO roles(description) VALUES('Employee');
